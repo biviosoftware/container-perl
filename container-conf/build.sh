adc5c6ef9e94cb79f9cdb23a566ab8e318055856
robnagler <github@q33.us>
2018-03-09 23:12:24 +0000

    SEGV caused by $PERLLIB, $BCONF being set

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 build_image_base=centos:7
 build_maintainer="Bivio Software <$build_type@bivio.biz>"
+export BIVIO_WANT_PERL=
 
 build_as_root() {
+    echo 'export BIVIO_WANT_PERL=' >> ~/.pre_bivio_bashrc
     curl radia.run | bash -s biviosoftware/container-perl
 }
 
 build_as_run_user() {
-    return
+    echo 'export BIVIO_WANT_PERL=' >> ~/.pre_bivio_bashrc
 }
