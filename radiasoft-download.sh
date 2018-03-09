c28ca224a17cc09b3b3f9ef20b0d3cd1b754cf8c
robnagler <github@q33.us>
2018-03-09 01:40:04 +0000

    include Crypt-Blowfish, not perl-Crypt-Eksblowfish

--- radiasoft-download.sh
+++ radiasoft-download.sh
@@ -78,7 +78,6 @@ container_perl_main() {
         perl-Convert-ASN1
         perl-Crypt-CBC
         perl-Crypt-DES
-        perl-Crypt-Eksblowfish
         perl-Crypt-IDEA
         perl-Crypt-OpenSSL-RSA
         perl-Crypt-OpenSSL-Random
@@ -203,6 +202,7 @@ container_perl_main() {
     done
     perl -pi -e 'm{local\(\$\[} && ($_ = q{})' /usr/share/perl5/*.pl
     cpan install OLLY/Search-Xapian-1.2.22.0.tar.gz
+    cpan install DPARIS/Crypt-Blowfish-2.14.tar.gz
     (
         install_download src/gmp-6.0.0a.tar.bz2 | tar xjf -
         cd gmp-6.0.0/demos/perl
