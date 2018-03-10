f669233d009f476da4d247ff774d8d2225baee44
robnagler <github@q33.us>
2018-03-10 01:51:07 +0000

    doc BIVIO_WANT_PERL=''

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 build_image_base=centos:7
 build_maintainer="Bivio Software <$build_type@bivio.biz>"
+# Perl and apps are installed globally so don't want a local
+# library conflicting.
 export BIVIO_WANT_PERL=
 
 build_as_root() {
