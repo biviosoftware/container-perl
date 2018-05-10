92b0737c1bcfa1455fec25613608b9cf22468281
robnagler <github@q33.us>
2018-05-10 23:15:37 +0000

    need bind for bivio-named

--- radiasoft-download.sh
+++ radiasoft-download.sh
@@ -228,7 +228,8 @@ container_perl_install_rest() {
         # POSIT: rsconf/component/postgrey.py sets etc
         ln --relative -s /srv/postgrey/etc /etc/postgrey
     )
-    install_yum_install ruby-devel
+    # fpm needs ruby & bind is needed for bivio-named
+    install_yum_install ruby-devel bind
     gem install fpm
 }
 
